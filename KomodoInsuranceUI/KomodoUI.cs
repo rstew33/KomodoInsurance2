e3f2942eee0ccdd1cc19fe872d6f6795191fdd9a
Andrew Steward <rstew33@gmail.com>
2020-11-24 20:14:07 -0500

    initial commit

---
+++ KomodoInsuranceUI/KomodoUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoInsuranceUI
+{
+    class KomodoUI
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
